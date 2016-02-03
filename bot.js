3f1c8ea0370f12a0184ea39f008e93473a30005f
Remco <SteamingMutt@users.noreply.github.com>
2016-02-03 22:51:56 +0100

    Wow, look how advanced we are now, wew

--- bot.js
+++ bot.js
@@ -1,4 +1,12 @@
-// Why didn't you add this
-
 var Discord = require('discord.js'),
-    bot = new Discord.Client();
+    bot = new Discord.Client(),
+    Commands = require('./commands.js'),
+    Config = require('./config.json');
+
+bot.on('message', function(msg) {
+  if (msg.content === "=ping") {
+    bot.sendMessage(msg.channel, "Pong!");
+  }
+});
+
+bot.login(Config.email, Config.password);
