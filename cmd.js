fa60c599f05f94bf57777344b3531455dbb962b8
OneMansGlory <onemansglory15@gmail.com>
2016-05-09 06:48:13 -0400

    Changed Join command to fit the bot account.
    Still need to fix config.

--- cmd.js
+++ cmd.js
@@ -82,26 +82,7 @@ exports.commands = {
   },
   "join": {
     process: function(bot, message, suffix) {
-      let query = suffix;
-      let sender = message.author.username;
-      if (!query) {
-        bot.sendMessage(message.channel, "Please specify an invite link.");
-        return;
-      }
-      let invite = message.content.split(" ")[1];
-      bot.joinServer(invite, function(error, server) {
-        if (error) {
-          bot.sendMessage(message.channel, "Something went wrong. Error code: " + error);
-        } else {
-          bot.sendMessage(message.channel, "Great! I just joined: " + server);
-          let messageArray = [];
-          messageArray.push("Hi! I'm **" + bot.user.username + "**. I was invited to this server by " + message.author + ".");
-          messageArray.push("You can use `" + trigger + "help` to see what I can do.");
-          messageArray.push("If you don't want me here, please use the " + AuthDetails.discordjs_trigger + "leave command to get me out.");
-          bot.sendMessage(server.defaultChannel, messageArray);
-          console.log("Joined server: " + server)
-        }
-      });
+    bot.sendMessage(msg.channel, "Apologies, but I am now a bot account. This means I cannot accept instant invites. Please go to my oAuth 2 link at " + config.oauthlink)
     }
   },
   "hello": {
